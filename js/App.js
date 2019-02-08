75596464fda561cda9f5c83f4213587881fa6ce1
Jakub Wiśniewski <mrkubasny@gmail.com>
2019-02-08 14:47:01 +0100

    v 0.03

--- js/App.js
+++ js/App.js
@@ -11,11 +11,11 @@ function generateTemplate(name, data, basicElement) {
 }
 
 /* ------------------API------------------------*/
-
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var baseUrl = prefix + 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+  'X-Client-Id': '3696',
+  'X-Auth-Token': '10db06a2ce89d0adfb5e9a8ac7ebd2ca'
 };
 
 fetch(baseUrl + '/board', { headers: myHeaders })
--- js/Column.js
+++ js/Column.js
@@ -2,52 +2,48 @@ function Column(id, name) {
   this.id = id;
   this.name = name;
   this.element = generateTemplate('column-template', { name: this.name, id: this.id });
-  if (event.target.classList.contains('add-card')) {
-	  var cardName = prompt("Enter the name of the card");
-	  event.preventDefault();
+  this.element.querySelector('.column').addEventListener('click', function (event) {
+	  if (event.target.classList.contains('btn-delete')) {
+	  	console.log(this);
+	      this.removeColumn();
+	  }
 
-	  fetch(baseUrl + '/card', {
-	      method: 'POST',
-	      body: {
-	        //body query
-	      }
-	    })
-	    .then(function(res) {
-	      return res.json();
-	    })
-	    .then(function() {
-	      	var data = new FormData();
-			data.append('name', cardName);
-			data.append('bootcamp_kanban_column_id', self.id);
+	  if (event.target.classList.contains('add-card')) {
+	         var cardName = prompt("Enter the name of the card");
+			  event.preventDefault();
 
-			fetch(baseUrl + '/card', {
-			    method: 'POST',
-			    headers: myHeaders,
-			    body: data,
-			  })
-			  .then(function(res) {
-			    return res.json();
-			  })
-			  .then(function(resp) {
-			    var card = new Card(resp.id, cardName);
-			    self.addCard(card);
-			  });
-	    });
+			  var data = new FormData();
+				data.append('name', cardName);
+				data.append('bootcamp_kanban_column_id', self.id);
 
-	  self.addCard(new Card(cardName));
-  }
+				fetch(baseUrl + '/card', {
+				    method: 'POST',
+				    headers: myHeaders,
+				    body: data,
+				  })
+				  .then(function(res) {
+				    return res.json();
+				  })
+				  .then(function(resp) {
+				    var card = new Card(resp.id, cardName);
+				    self.addCard(card);
+				  });
+
+			  self.addCard(new Card(cardName));
+	  }
+	});
 
   this.addCard = (card) => {
   	this.element.querySelector('ul').appendChild(card.element)
-  }
-  this.removeColumn = () => {
-  	var self = this;
-  fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
-    .then(function(resp) {
-      return resp.json();
-    })
-    .then(function(resp) {
-      self.element.parentNode.removeChild(self.element);
-    });
-  }
+  }	
+   this.removeColumn = () => {
+	  fetch(baseUrl + '/column/' + this.id, { method: 'DELETE', headers: myHeaders })
+	    .then(function(resp) {
+	      return resp.json();
+	    })
+	    .then(function(resp) {
+	      this.element.parentNode.removeChild(this.element);
+	    });
+	}
+
 }
